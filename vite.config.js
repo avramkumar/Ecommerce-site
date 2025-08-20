8902db00912fd6dca7c4a2fc0a4eb3933cfcff95
ram <ramkumarangajala1219@gmail.com>
2025-08-20 10:46:41 +0530

    Fix index.html paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ecommerce/', // <--- Add this
+  base: '/Ecommerce-site/', // <--- Add this
   plugins: [react()],
 });
