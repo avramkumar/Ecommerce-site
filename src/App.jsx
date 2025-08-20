c14b67aa05772d407e6ba499415022ea185f57ff
ram <ramkumarangajala1219@gmail.com>
2025-08-20 10:56:20 +0530

    Fix: added basename for BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/Ecommerce-site">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
